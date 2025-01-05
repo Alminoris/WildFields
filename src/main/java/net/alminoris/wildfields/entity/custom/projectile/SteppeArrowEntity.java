09bcd568ff73ae582b53d30eb851c6c3b67240d0
alminoris <olegasaulov.prog@gmail.com>
2025-01-05 21:31:52 +0200

    Added the mechanics of the steppe arrow

--- src/main/java/net/alminoris/wildfields/entity/custom/projectile/SteppeArrowEntity.java
+++ src/main/java/net/alminoris/wildfields/entity/custom/projectile/SteppeArrowEntity.java
@@ -2,6 +2,7 @@ package net.alminoris.wildfields.entity.custom.projectile;
 
 import net.alminoris.wildfields.entity.ModEntities;
 import net.alminoris.wildfields.item.ModItems;
+import net.alminoris.wildfields.sound.ModSounds;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
@@ -48,7 +49,9 @@ public class SteppeArrowEntity extends PersistentProjectileEntity
 
         if (this.getOwner() instanceof LivingEntity shooter)
         {
-
+            playSound(ModSounds.SOUND_STEPPE_EAGLE_DEATH, 1.5f, 1.0f);
+            StatusEffectInstance statusEffectInstance = new StatusEffectInstance(StatusEffects.SLOWNESS, 200, 3);
+            target.addStatusEffect(statusEffectInstance);
         }
     }
 
