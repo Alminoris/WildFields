913eb84033604748290219076ee3a01d0a9c6be9
AOA <aoa@d.co>
2025-04-10 07:48:08 +0300

    #Update 1.1.0. Steppes Villages

--- src/main/java/net/alminoris/wildfields/world/gen/decorator/custom/CustomAlterGroundTreeDecorator.java
+++ src/main/java/net/alminoris/wildfields/world/gen/decorator/custom/CustomAlterGroundTreeDecorator.java
@@ -10,7 +10,6 @@ import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.stateprovider.BlockStateProvider;
 import net.minecraft.world.gen.treedecorator.TreeDecorator;
 import net.minecraft.world.gen.treedecorator.TreeDecoratorType;
-
 import java.util.List;
 import java.util.Random;
 
